828c735ff595efac4a8001171e43049e9bd78d7c
ykersha <youssef_kersha@rocketmail.com>
2021-10-30 21:21:01 +0200

    added book router

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+const bookRouter = require("./api/books")
+
+app.use("/api/books", bookRouter);
+
+
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
