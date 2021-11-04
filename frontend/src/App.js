aa512fb4f8a056eb89537eeeb2631409f218ae25
mayhussien <92927054+mayhussien@users.noreply.github.com>
2021-11-04 22:12:49 +0200

    add updateFlight

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-
-=======
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,26 +10,9 @@ import Home from './Pages/Home.Page';
 import Test from './Pages/Test.page';
 import CreateFlight from './Components/CreateFlight';
 import UpdateFlight from './Components/updateFlight';
->>>>>>> Stashed changes
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Router>
         <Switch>
           <Route exact path="/"
@@ -45,7 +25,7 @@ function App() {
            <Route path='/update-flight' component={UpdateFlight} />
         </Switch>
       </Router>
->>>>>>> Stashed changes
+
     </div>
   );
 }
