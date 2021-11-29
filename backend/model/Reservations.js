ed39cc331a56b18cd33df8ba17596b11f7254454
mayhussien <92927054+mayhussien@users.noreply.github.com>
2021-11-29 14:57:08 +0200

    changes

--- backend/model/Reservations.js
+++ backend/model/Reservations.js
@@ -3,6 +3,10 @@
 const mongoose = require('mongoose');
 
 const ReservationSchema = new mongoose.Schema({
+  userID:{
+      type:String,
+      required:true
+  },
   from: {
     type: Number,
     required: true
@@ -11,6 +15,11 @@ const ReservationSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
+  cabin:{
+    type:String ,
+    required:true
+  }
+
 });
 
 module.exports = Reservation = mongoose.model('reservation', ReservationSchema);
