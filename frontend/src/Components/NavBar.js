7afe7906eedd7f87114eb201e56968a499f1b43c
ykersha <youssef_kersha@rocketmail.com>
2021-12-03 22:40:44 +0200

    login

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <AppBar position="static" style={{ alignItems: 'flex-start' }}>
       <Toolbar>
-        <Button className="admin" color="inherit" onClick={(e) => history.push('/user-details/' + localStorage.getItem("userId"))}>View Profile</Button>
+      {userId ?  <Button className="admin" color="inherit" onClick={(e) => history.push('/user-details/' + localStorage.getItem("userId"))}>View Profile</Button>:null}
         <Button className="admin" color="inherit" onClick={(e) => history.push('/Reserved-flights')}>View Reservations</Button>
         <Button className="admin" color="inherit" onClick={(e) => history.push('/')}>Search for Flights</Button>
 
