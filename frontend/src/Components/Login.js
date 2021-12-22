cf4b2c3df9834ad8d762d6d0dd58d7d1551b4905
mayhussien <92927054+mayhussien@users.noreply.github.com>
2021-12-22 21:50:57 +0200

    login frontend

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -2,6 +2,19 @@ import {Link} from "react-router-dom";
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { BACKEND_URL } from '../API/URLS'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Login=()=> {
 	const [email, setEmail] = useState('')
@@ -29,10 +42,12 @@ const Login=()=> {
             localStorage.setItem("user",data.user)
             localStorage.setItem("userId",data.user.userId)
 			window.location.href = '/'
+            console.log("i am here")
 		} else {
 			alert('Invalid Email or Password')
 		}
 	}
+    const theme = createTheme();
 
 	return (
 		<div>
@@ -58,6 +73,99 @@ const Login=()=> {
 			</form>
 		</div>
 	)
+    // return (
+    //     <ThemeProvider theme={theme}>
+    //       <Grid container component="main" sx={{ height: '100vh' }}>
+    //         <CssBaseline />
+    //         <Grid
+    //           item
+    //           xs={false}
+    //           sm={4}
+    //           md={7}
+    //           sx={{
+    //             backgroundImage: 'url(https://source.unsplash.com/random)',
+    //             backgroundRepeat: 'no-repeat',
+    //             backgroundColor: (t) =>
+    //               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+    //             backgroundSize: 'cover',
+    //             backgroundPosition: 'center',
+    //           }}
+    //         />
+    //         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+    //           <Box
+    //             sx={{
+    //               my: 8,
+    //               mx: 4,
+    //               display: 'flex',
+    //               flexDirection: 'column',
+    //               alignItems: 'center',
+    //             }}
+    //           >
+    //             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+    //               <LockOutlinedIcon />
+    //             </Avatar>
+    //             <Typography component="h1" variant="h5">
+    //               Sign in
+    //             </Typography>
+    //             <Box component="form" noValidate onSubmit={loginUser} sx={{ mt: 1 }}>
+    //               <TextField
+    //                 margin="normal"
+    //                 required
+    //                 fullWidth
+    //                 id="email"
+    //                 label="Email Address"
+    //                 name="email"
+    //                 autoComplete="email"
+    //                 value={email}
+	//      			onChange={(e) => setEmail(e.target.value)}
+	// 				type="email"
+	// 				placeholder="Email"
+    //                 autoFocus
+    //               />
+    //               <TextField
+    //                 margin="normal"
+    //                 required
+    //                 fullWidth
+    //                 name="password"
+    //                 label="Password"
+    //                 type="password"
+    //                 id="password"
+    //                 autoComplete="current-password"
+    //                 value={password}
+    //   				onChange={(e) => setPassword(e.target.value)}
+	//     			type="password"
+	// 				placeholder="Password"
+    //               />
+    //               <FormControlLabel
+    //                 control={<Checkbox value="remember" color="primary" />}
+    //                 label="Remember me"
+    //               />
+    //               <Button
+    //                 type="submit"
+    //                 fullWidth
+    //                 variant="contained"
+    //                 sx={{ mt: 3, mb: 2 }}
+    //               >
+    //                 Sign In
+    //               </Button>
+    //               <Grid container>
+    //                 <Grid item xs>
+    //                   <Link href="#" variant="body2">
+    //                     Forgot password?
+    //                   </Link>
+    //                 </Grid>
+    //                 <Grid item>
+    //                   <Link href="#" variant="body2">
+    //                     {"Don't have an account? Sign Up"}
+    //                   </Link>
+    //                 </Grid>
+    //               </Grid>
+    //             </Box>
+    //           </Box>
+    //         </Grid>
+    //       </Grid>
+    //     </ThemeProvider>
+    //   );
 }
 
 export default Login
