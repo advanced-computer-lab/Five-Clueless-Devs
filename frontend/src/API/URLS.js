130aa82c6247b76a0eb51cd2d6e22dca4fb6e44f
ykersha <youssef_kersha@rocketmail.com>
2021-12-25 17:43:44 +0200

    title and icon

--- frontend/src/API/URLS.js
+++ frontend/src/API/URLS.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'http://localhost:8082/api/';
+//export const BACKEND_URL = 'http://localhost:8082/api/';
+
+export const BACKEND_URL = 'https://cluelessdevsback.herokuapp.com/api/';
